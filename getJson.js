d493ab9494604a00a085cea4f760eec8d3b27c15
BoldErdene1221 <azu.wert@gmail.com>
2020-05-24 18:21:39 +0700

    add setInterval method

--- getJson.js
+++ getJson.js
@@ -1,33 +1,59 @@
-export default function getPosts(url, callMeBack) {
+export default function getPosts(url, element, callMeBack) {
+  setInterval(async () => {
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            let contents = [];
-            data.content.forEach((content) => {
-                contents.push(new GobiWomen(content.name, content.title1, content.img1, content.title2, content.img2, content.title3, content.img3, content.title4, content.img4, 0))
-            })
-            callMeBack(contents);
-            console.log(data.content)
-        })
+      .then((response) => response.json())
+      .then((data) => {
+        let contents = [];
+        data.content.forEach((content) => {
+          contents.push(
+            new GobiWomen(
+              content.name,
+              content.title1,
+              content.img1,
+              content.title2,
+              content.img2,
+              content.title3,
+              content.img3,
+              content.title4,
+              content.img4,
+              0
+            )
+          );
+        });
+        callMeBack(contents);
+        console.log("hello from getJSON");
+        console.log(data.content);
+      });
+    element.innerHTML = " ";
+  }, 5000);
+  //   document.getElementById("emegtei").innerHTML = " ";
 }
 
 export class GobiWomen {
+  constructor(
+    _name,
+    _title1,
+    _img1,
+    _title2,
+    _img2,
+    _title3,
+    _img3,
+    _title4,
+    _img4
+  ) {
+    this.name = _name;
+    this.title1 = _title1;
+    this.title2 = _title2;
+    this.title3 = _title3;
+    this.title4 = _title4;
+    this.img1 = _img1;
+    this.img2 = _img2;
+    this.img3 = _img3;
+    this.img4 = _img4;
+  }
 
-    constructor(_name, _title1, _img1, _title2, _img2, _title3, _img3, _title4, _img4) {
-        this.name = _name;
-        this.title1 = _title1;
-        this.title2 = _title2;
-        this.title3 = _title3;
-        this.title4 = _title4;
-        this.img1 = _img1;
-        this.img2 = _img2;
-        this.img3 = _img3;
-        this.img4 = _img4;
-    }
-
-    Create() {
-
-        return `
+  Create() {
+    return `
         <h3>${this.name}</h3>
 			<div class="grid-container">
 				<a href=""><img src="${this.img1}" alt=""></a>
@@ -40,8 +66,6 @@ export class GobiWomen {
 				<a href="">${this.title2}</a>
 				<a href="">${this.title3}</a>
 				<a href="">${this.title4}</a>
-			</div>`
-
-    }
-
+			</div>`;
+  }
 }
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import getPosts from "./getJson.js"
+import getPosts from "./getJson.js";
 const songoltUrl = "https://api.jsonbin.io/b/5ec9530cbbaf1f258945a769/latest";
-getPosts(songoltUrl,
-    (contents) => {
-        contents.forEach(
-            (content) => {
-                document.getElementById("emegtei").innerHTML += content.Create();
-            })
-    })
+const element = document.getElementById("emegtei");
+getPosts(songoltUrl, element, (contents) => {
+  contents.forEach((content) => {
+    document.getElementById("emegtei").innerHTML += content.Create();
+  });
+});
